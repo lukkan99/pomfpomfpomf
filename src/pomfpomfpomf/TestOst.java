7bcc8e7cf8c0ed9a271a0422bdb07f451f3c561f
lukekb <lukekb@E11031.elevad.umea.se>
2014-09-15 10:16:41 +0200

    fixed a vary vary smail erorr

--- src/pomfpomfpomf/TestOst.java
+++ src/pomfpomfpomf/TestOst.java
@@ -6,7 +6,7 @@ import org.spongepowered.api.event.state.ServerStartingEvent;
 import org.spongepowered.api.plugin.Plugin;
 
 
-@Plugin(id = "PomfPomfPOmf", name = "PomfPomfPomfLukkan99sTest")
+@Plugin(id = "PomfPomfPomf", name = "PomfPomfPomfLukkan99sTest")
 public class TestOst {
     @SpongeEventHandler
     public void onInitialization(InitializationEvent event) {
